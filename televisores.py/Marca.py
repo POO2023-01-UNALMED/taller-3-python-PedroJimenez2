4d0b8c468e6747bb129b098c6378afa2f1dd286c
PedroJimenez2 <95315108+PedroJimenez2@users.noreply.github.com>
2023-02-27 23:06:53 -0500

    commit3

--- televisores.py/Marca.py
+++
@@ -1,9 +0,0 @@
-class Marca:
-    def __init__(self,nombre):
-        self._nombre = nombre
-
-    def setNombre(self,nom):
-        self._nombre = nom
-
-    def getNombre(self):
-        return self._nombre
--- televisores.py/televisores.py
+++ televisores.py/televisores.py
@@ -1,3 +1,13 @@
+class Marca:
+    def __init__(self,nombre):
+        self._nombre = nombre
+
+    def setNombre(self,nom):
+        self._nombre = nom
+
+    def getNombre(self):
+        return self._nombre
+    
 class TV:
     _numTV = 0
     def __init__(self,marca,estado):
@@ -78,3 +88,38 @@ class TV:
         if self._volumen > 0 and self._volumen <= 7:
             if self._estado == True:
                 self._volumen -= 1
+
+class Control:
+    def __init__(self):
+        self._tv = None
+
+    def turnOn(self):
+        self._tv.setEstado(True)
+
+    def turnOff(self):
+        self._tv.setEstado(False)
+
+    def canalUp(self):
+        self._tv.canalUp()
+
+    def canalDown(self):
+        self._tv.canalDown()
+
+    def volumenUp(self):
+        self._tv.volumenUp()
+
+    def volumenDown(self):
+        self._tv.volumenDown()
+
+    def setCanal(self,canal):
+        self._tv.setCanal(canal)
+
+    def enlazar(self,telev):
+        self._tv = telev
+        telev.setControl(self)
+
+    def setTv(self,t):
+        self._tv = t
+
+    def getTv(self):
+        return self._tv
